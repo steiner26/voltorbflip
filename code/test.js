f0ff8a5cdbc27d470a41ac48466ea91ae883a899
Brandon Stein <bs645@cornell.edu>
2018-12-25 00:01:28 -0500

    Update test.js

--- code/test.js
+++ code/test.js
@@ -2,6 +2,6 @@ alert("hello");
 
 var data = ""
 $.getJSON( "code/levels.json", function( json ) {
-  data = json[2][chance.natural({ min: 0, max: 4 })]
+  data = json["2"][chance.natural({ min: 0, max: 4 })]
 });
 console.log(data)
