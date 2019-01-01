6eebd317ecb767a073dfd8f9a391d13dfd0c2e66
Brandon Stein <bs645@cornell.edu>
2019-01-01 17:29:52 -0500

    fixed click manager

--- code/htmlManager.js
+++ code/htmlManager.js
@@ -8,7 +8,7 @@ function HTMLManager () {
 HTMLManager.prototype.setBoard = function (board) {
   for  (r = 0; r < 5; r++) {
     for (c = 0; c < 5; c++) {
-      var tile = this.boardContainer.childNodes[r].childNodes[c]
+      var tile = this.boardContainer.children[r].children[c]
       tile.click(function() {
         board.flip(r, c);
       });
