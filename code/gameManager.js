cab95275c9f11708882cd023471add78c8be5819
Brandon Stein <bs645@cornell.edu>
2019-01-21 12:28:09 -0500

    changed tile flipping classes

--- code/gameManager.js
+++ code/gameManager.js
@@ -7,12 +7,11 @@ function GameManager (Manager) {
 GameManager.prototype.flip = function (pos) {
   result = this.board.flip(pos)
   if (result == 0) {
-    console.log("bomb");
     //Game is over
   } else if (result != null) {
-    console.log(result)
     //Flip the tile
   }
+  return result;
 }
 
 GameManager.prototype.getTile = function (row, col) {
--- code/htmlManager.js
+++ code/htmlManager.js
@@ -10,10 +10,27 @@ HTMLManager.prototype.setBoard = function (manager) {
     for (c = 0; c < 5; c++) {
       var tile = this.boardContainer.children[r].children[c];
       tile.pos = {row:r, col:c};
-      tile.children[1].innerHTML = manager.getTile(r, c).toString();
       tile.addEventListener("click", function () {
         this.classList.add('is-flipped');
-        manager.flip(this.pos);
+        if (value = manager.flip(this.pos) == 0) {
+          this.children[1].classList.add("tile_back_bomb");
+        } else {
+          this.children[1].classList.add("tile_back_"+value.toString());
+        }
+        // switch (manager.flip(this.pos)) {
+        //   case 0:
+        //     this.children[1].classList.add("tile-bomb");
+        //     break;
+        //   case 1:
+        //     this.children[1].classList.add("tile-1");
+        //     break;
+        //   case 2:
+        //     this.children[1].classList.add("tile-2");
+        //     break;
+        //   case 3:
+        //     this.children[1].classList.add("tile-3");
+        //     break;
+        // }
       });
     }
   }
