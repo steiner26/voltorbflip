f34e0bd58f9723ae79afd454098c1df6b177e3be
Brandon Stein <bs645@cornell.edu>
2019-01-21 17:53:37 -0500

    Added code for completing a board

--- code/gameManager.js
+++ code/gameManager.js
@@ -1,19 +1,32 @@
 function GameManager (Manager) {
   this.htmlManager = new Manager;
-  this.board = new Board(1);
+  this.level = 1
+  this.board = new Board(this.level);
   this.htmlManager.setBoard(this);
+  this.totalPoints = 0;
+  this.currentPoints = 1;
 }
 
 GameManager.prototype.flip = function (pos) {
   result = this.board.flip(pos)
   if (result == 0) {
-    //Game is over
+    //Game Lost
   } else if (result != null) {
-    //Flip the tile
+    this.currentPoints *= result;
+    if (this.currentPoints == this.totalPoints) {
+      this.gameWon();
+    }
   }
   return result;
 }
 
+GameManager.prototype.gameWon = function () {
+  this.level = Math.max(this.level+1, 7);
+  this.board = new Board(this.level);
+  this.htmlManager.setBoard(this);
+}
+
+//Might not need
 GameManager.prototype.getTile = function (row, col) {
   return this.board.tiles[row][col].value
 }
--- code/htmlManager.js
+++ code/htmlManager.js
@@ -10,6 +10,7 @@ HTMLManager.prototype.setBoard = function (manager) {
     for (c = 0; c < 5; c++) {
       var tile = this.boardContainer.children[r].children[c];
       tile.pos = {row:r, col:c};
+      tile.classList.remove("is-flipped");
       tile.addEventListener("click", function () {
         this.classList.add('is-flipped');
         if ((value = manager.flip(this.pos)) == 0) {
