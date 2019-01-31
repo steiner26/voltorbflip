f485bf8290a78dcb7f2dd04169113dc03b83538a
Brandon Stein <bstein179@gmail.com>
2019-01-31 12:08:59 -0500

    Adjusted timing for next level msg

--- code/htmlManager.js
+++ code/htmlManager.js
@@ -99,7 +99,7 @@ HTMLManager.prototype.displayNextLevel = function (nextlevel, difference, manage
     $(".game-message").toggleClass("fadeIn fadeOut");
     setTimeout(function () {
       $(".game-message").toggleClass("fadeOut animated hidden");
-    }, 800)
+    }, 900)
     hm.addListeners(manager);
-  }, 1000);
+  }, 1500);
 }
